e5c2bcf7707601bcb12663dd8bccc086393051cb
VtheBroken <oykubalaban5@gmail.com>
2023-10-29 18:07:59 +0300

    health ve merge yapıldı sadece hostta sorun var

--- Chatbotproject2/actions/actions.py
+++ Chatbotproject2/actions/actions.py
@@ -1,27 +1,25 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+
+from typing import Any, Text, Dict, List
+
+import rasa_sdk
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionPerson(rasa_sdk.Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_person"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: rasa_sdk.Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        person_entity = next(tracker.get_latest_entity_values('person'),None)
+
+        if person_entity:
+            dispatcher.utter_message(text=f"Hi {person_entity} how can i help you?")
+        else:
+            dispatcher.utter_message(text=f"I'm sorry,something went wrong")
+
+
+         return []
